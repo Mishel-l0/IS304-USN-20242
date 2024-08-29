77c037f06a668d6e6da18749466b15216e1c07ce
Evelyna-0 <evelsmoralesb@unimonserrate.edu.co>
2024-08-28 20:02:47 -0500

    Def atributos

--- SESION03/taller.py
+++ SESION03/taller.py
@@ -3,3 +3,27 @@ Crear un programa que permita crear objetos de la clase CuentaBancaria, cuyos at
 Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
 Utilizar un menu para las diferentes opciones, tales como aperturaCta, consignar, retirar y transferencia entre otros.
 '''
+'''
+Evelyn Samara Morales Bohorquez
+Unimonserrate
+Jornada nocturna
+Estructura de datos
+'''
+
+class CuentaBancaria:
+    def __init__(self, numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion):
+        self.__numeroCta = numeroCta
+        self.__nombreCliente = nombreCliente
+        self.__saldoCta = saldoCta
+        self.__fechaApertura = fechaApertura
+        self.__ultimoRetiro = ultimoRetiro
+        self.__ultimaConsignacion = ultimaConsignacion
+        
+    def set (self, numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion):
+        self.__numeroCta = numeroCta
+        self.__nombreCliente = nombreCliente
+        self.__saldoCta = saldoCta
+        self.__fechaApertura = fechaApertura
+        self.__ultimoRetiro = ultimoRetiro
+        self.__ultimaConsignacion = ultimaConsignacion
+        
